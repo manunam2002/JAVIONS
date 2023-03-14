aa0421df2d85ab25807dec279aa9078e859c82a3
Manu Cristini <manu.cristini@epfl.ch>
2023-03-14 10:05:09 +0100

    14.03.2023

--- Javions/src/ch/epfl/javions/adsb/AircraftIdentificationMessage.java
+++ Javions/src/ch/epfl/javions/adsb/AircraftIdentificationMessage.java
@@ -2,39 +2,53 @@ package ch.epfl.javions.adsb;
 
 import ch.epfl.javions.aircraft.IcaoAddress;
 
-public record AircraftIdentificationMessage() implements Message {
-    public static long timeStampNs;
-    public static IcaoAddress icaoAddress;
-    public static int category;
-    public static CallSign callSign;
+public record AircraftIdentificationMessage(long timeStampNs, IcaoAddress icaoAddress,
+        int category, CallSign callSign) implements Message {
 
     public AircraftIdentificationMessage{
-
+        if (icaoAddress == null || callSign == null) throw new NullPointerException();
+        if (timeStampNs < 0) throw new IllegalArgumentException();
     }
 
-
-
-
     public static AircraftIdentificationMessage of(RawMessage rawMessage){
-
-        return null;
+        int category1 = (rawMessage.bytes().byteAt(4)) & 0x7;
+        int category2 = (14-rawMessage.typeCode())<<4;
+        category1 = category2 | category1;
+        StringBuilder callSign1 = new StringBuilder();
+        long callSignValue = rawMessage.bytes().bytesInRange(5,11);
+        for (int i = 0 ; i < 8 ; ++i){
+            int c = (int) ((callSignValue >>> ((7-i)*6)) & 0x3F);
+            char c1;
+            if (c > 0 && c < 27){
+                c1 = (char)(c +64);
+            } else {
+                if ((c > 47 && c < 58)||(c == 32)){
+                    c1 = (char)c;
+                } else {
+                    return null;
+                }
+            }
+            callSign1.append(c1);
+        }
+        return new AircraftIdentificationMessage(rawMessage.timeStampNs(),rawMessage.icaoAddress(),
+                category1,new CallSign(callSign1.toString()));
     }
 
     @Override
     public long timeStampNs() {
-        return 0;
+        return timeStampNs;
     }
 
     @Override
     public IcaoAddress icaoAddress() {
-        return null;
+        return icaoAddress;
     }
 
     public CallSign callSign() {
-        return null;
+        return callSign;
     }
 
     public int category() {
-        return 0;
+        return category;
     }
 }
--- Javions/src/ch/epfl/javions/adsb/CprDecoder.java
+++ Javions/src/ch/epfl/javions/adsb/CprDecoder.java
@@ -1,10 +1,49 @@
 package ch.epfl.javions.adsb;
 
 import ch.epfl.javions.GeoPos;
+import ch.epfl.javions.Units;
 
 public class CprDecoder {
-    public static GeoPos decodePosition(double x0, double y0, double x1, double y1, int mostRecent){
 
+    private static final int latZones0 = 60;
+    private static final int latZones1 = 59;
+
+    private static final double lonZonesCalculator = 1 - Math.cos(2 * Math.PI * (1.0/latZones0));
+
+    public static GeoPos decodePosition(double x0, double y0, double x1, double y1, int mostRecent){
+        if ((mostRecent != 0) && (mostRecent != 1)) throw new IllegalArgumentException();
+        double latZone = Math.rint(y0*latZones1 - y1*latZones0);
+        double latZone0;
+        double latZone1;
+        if (latZone < 0){
+            latZone0 = latZone+latZones0;
+            latZone1 = latZone+latZones1;
+        } else {
+            latZone0 = latZone;
+            latZone1 = latZone;
+        }
+        double latZone0Rad = Units.convertFrom(latZone0,Units.Angle.TURN);
+        double lat0 = (1.0/latZones0) * (latZone0 + y0);
+        double lat1 = (1.0/latZones1) * (latZone1 + y1);
+        double lonZones = Math.acos(1 - (lonZonesCalculator / (Math.cos(latZone0Rad) * Math.cos(latZone0Rad))));
+        double lonZones0 = Math.floor(2 * Math.PI / lonZones);
+        double lonZones1 = lonZones0 -1;
+        double lonZone = Math.rint(x0*lonZones1 - x1*lonZones0);
+        double lonZone0;
+        double lonZone1;
+        if (lonZone < 0){
+            lonZone0 = lonZone+lonZones0;
+            lonZone1 = lonZone+lonZones1;
+        } else {
+            lonZone0 = lonZone;
+            lonZone1 = lonZone;
+        }
+        double lon0 = (1.0/lonZones0) * (lonZone0 + x0);
+        double lon1 = (1.0/lonZones1) * (lonZone1 + x1);
+        if (mostRecent == 0){
+            //return new GeoPos(Units.convert(lon0,Units.Angle.TURN,Units.Angle.T32),
+            //        Units.convert(lat0,Units.Angle.TURN,Units.Angle.T32));
+        }
         return null;
     }
 }

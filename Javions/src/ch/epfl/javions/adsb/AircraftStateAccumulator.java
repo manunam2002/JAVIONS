53ab4ec0fe71913a974650411dc2048bdf83c7ac
Manu Cristini <manu.cristini@epfl.ch>
2023-03-21 10:03:26 +0100

    21.03.2023

--- Javions/src/ch/epfl/javions/adsb/AircraftStateAccumulator.java
+++ Javions/src/ch/epfl/javions/adsb/AircraftStateAccumulator.java
@@ -1,16 +1,48 @@
 package ch.epfl.javions.adsb;
 
-public class AircraftStateAccumulator<T> {
-    AircraftStateAccumulator(T stateSetter){
+import ch.epfl.javions.GeoPos;
 
+public class AircraftStateAccumulator <T extends AircraftStateSetter> {
 
+    private T stateSetter;
+
+    private AirbornePositionMessage lastPositionMessage;
+
+    public AircraftStateAccumulator(T stateSetter){
+        if (stateSetter == null) throw new NullPointerException();
+        this.stateSetter = stateSetter;
     }
+
     public T stateSetter(){
-        return stateSetter();
+        return stateSetter;
     }
-     public void update(Message message){
-
-     }
+    public void update(Message message){
+        switch (message){
+            case AircraftIdentificationMessage aim -> {
+                stateSetter.setLastMessageTimeStampNs(aim.timeStampNs());
+                stateSetter.setCategory(aim.category());
+                stateSetter.setCallSign(aim.callSign());
+            }
+            case AirbornePositionMessage aim -> {
+                stateSetter.setLastMessageTimeStampNs(aim.timeStampNs());
+                stateSetter.setAltitude(aim.altitude());
+                if (aim.timeStampNs()-lastPositionMessage.timeStampNs() <= 10000){
+                    switch (aim.parity()){
+                        case 1 -> stateSetter.setPosition(CprDecoder.decodePosition(lastPositionMessage.x(),
+                                lastPositionMessage.y(), aim.x(), aim.y(),1));
+                        default -> stateSetter.setPosition(CprDecoder.decodePosition(aim.x(), aim.y(),
+                                lastPositionMessage.x(), lastPositionMessage.y(), 0));
+                    }
+                }
+            }
+            case AirborneVelocityMessage aim -> {
+                stateSetter.setLastMessageTimeStampNs(aim.timeStampNs());
+                stateSetter.setVelocity(aim.speed());
+                stateSetter.setTrackOrHeading(aim.trackOrHeading());
+            }
+            default -> {}
+        }
+    }
 
 
 
--- Javions/src/ch/epfl/javions/adsb/MessageParser.java
+++ Javions/src/ch/epfl/javions/adsb/MessageParser.java
@@ -1,19 +1,20 @@
 package ch.epfl.javions.adsb;
 
+import ch.epfl.javions.Units;
 import ch.epfl.javions.aircraft.IcaoAddress;
 
 public class MessageParser {
-     public static Message parse(RawMessage rawMessage){
-         return new Message() {
-             @Override
-             public long timeStampNs() {
-                 return 0;
-             }
-
-             @Override
-             public IcaoAddress icaoAddress() {
-                 return null;
-             }
-         };
-         }
-     }
+    public static Message parse(RawMessage rawMessage) {
+        if (rawMessage.typeCode() > 0 && rawMessage.typeCode() < 5) {
+            return AircraftIdentificationMessage.of(rawMessage);
+        }
+        if ((rawMessage.typeCode() > 8 && rawMessage.typeCode() < 19) ||
+                (rawMessage.typeCode() > 19 && rawMessage.typeCode() < 23)) {
+            return AirbornePositionMessage.of(rawMessage);
+        }
+        if (rawMessage.typeCode() == 19) {
+            return AirborneVelocityMessage.of(rawMessage);
+        }
+        return null;
+    }
+}
